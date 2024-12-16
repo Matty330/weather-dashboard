077d7b5c02526f8883bebb5cd433c42c4a5ed09c
Matt <mwelc64@yahoo.com>
2024-12-16 08:43:02 -0500

    Add placeholder route functionality and set up server basics

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,11 +9,16 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-// TODO: Serve static files of entire client dist folder
+import path from 'path';
+//  Serve static files of entire client dist folder
+app.use(express.static(path.join(__dirname, '../../client/dist')));
 
-// TODO: Implement middleware for parsing JSON and urlencoded form data
+//  Implement middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 
-// TODO: Implement middleware to connect the routes
+import routes from './routes/index.js';
+//  Implement middleware to connect the routes
 app.use(routes);
 
 // Start the server on the port
